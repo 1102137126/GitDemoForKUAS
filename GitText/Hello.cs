2034af2ca14adb9e204f75019f93a4e772651f5d
1102137126 <1102137126@gm.kuas.edu.tw>
2016-03-03 15:00:37 +0800

    hello

---
+++ GitText/Hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitText
+{
+    public class Hello
+    {
+        public static void Main() {
+            System.Console.WriteLine("Hello!!");
+            System.Console.ReadLine();
+        }
+    }
+}
